02105d19c208e84339192eee1bb5eaeaf0ed0806
robaji <110429393+robaji@users.noreply.github.com>
2022-10-12 16:32:20 -0400

    integrated components+style

--- social-media-angular/src/app/components/reset-password/reset-password.component.ts
+++ social-media-angular/src/app/components/reset-password/reset-password.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { ResetPasswordService } from '../services/reset-password.service';
+import { ResetPasswordService } from '../../services/reset-password.service'
 
 @Component({
   selector: 'app-reset-password',
