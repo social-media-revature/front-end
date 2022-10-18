9ce59d2b0a93e907bb5a8e25e98e8798b78f13db
robaji <110429393+robaji@users.noreply.github.com>
2022-10-18 14:26:02 -0400

    updated URL for ec2 instance

--- social-media-angular/src/environments/environment.prod.ts
+++ social-media-angular/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   withCredentials: true,
-  baseUrl: "http://ec2-54-163-48-95.compute-1.amazonaws.com:8081",
+  baseUrl: "ec2-3-80-154-253.compute-1.amazonaws.com:8081",
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': 'http://mrbucket456.s3.amazonaws.com',
