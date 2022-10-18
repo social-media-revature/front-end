ce32173144f7be32b9bba7870731877254b8091e
robaji <110429393+robaji@users.noreply.github.com>
2022-10-18 14:15:58 -0400

    updated URL for s3 bucket

--- social-media-angular/src/environments/environment.prod.ts
+++ social-media-angular/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   withCredentials: true,
-  baseUrl: "http://ec2-3-80-154-253.compute-1.amazonaws.com",
+  baseUrl: "http://ec2-54-163-48-95.compute-1.amazonaws.com:8081",
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'http://mrbucket45678.s3.amazonaws.com',
+    'Access-Control-Allow-Origin': 'http://mrbucket456.s3.amazonaws.com',
   },
 };
